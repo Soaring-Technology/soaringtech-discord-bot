1d5449f8f4c53f6d8ed9168fc4807a4580316774
munhyunsu <munhyunsu@gmail.com>
2024-08-18 23:05:11 +0900

    base code done

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-import express from 'express';
+require('dotenv').config();
+const express = require('express');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.EXPRESS_APP_PORT || 3000;
+const router = express.Router();
 
-app.listen(PORT, () => {
-  console.log('Listening in port', port);
+
+router.get('/', (req, res) => {
+  res.json({'message': 'Hello, world!'});
+});
+
+
+app.use(`${process.env.EXPRESS_APP_URL_PREFIX}`, router);
+
+app.listen(port, () => {
+  console.log(`Listening in port ${port}`);
 });
 
