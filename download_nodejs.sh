81c1c7b41409c346c0d244b7f430711a7162a8e1
munhyunsu <munhyunsu@gmail.com>
2024-08-24 09:11:52 +0900

    Download nodejs.tar.xz

--- download_nodejs.sh
+++ download_nodejs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://nodejs.org/dist/v20.16.0/node-v20.16.0-linux-x64.tar.xz
-tar -xvf node-v20.16.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz -O nodejs.tar.xz
+tar -xvf nodejs.tar.xz
 
