c5e5fa21c7a5e0667992c49e8ed4798accb1ecca
munhyunsu <munhyunsu@gmail.com>
2024-08-30 17:46:47 +0900

    deactivate

--- enable_environments.sh
+++ enable_environments.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+export OLD_PATH=$PATH
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export PATH=${DIR}/nodejs/bin:${PATH}
+
+deactivate() {
+    export PATH=$OLD_PATH
+    unset OLD_PATH
+    unset -f deactivate
+}
+
+echo "Environment activated. Use 'deactivate' to revert."
 
