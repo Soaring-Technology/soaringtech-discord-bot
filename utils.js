c8b4ddd055bcd55d800375ecd4e6c1c3b79ab667
munhyunsu <munhyunsu@gmail.com>
2024-08-31 10:27:59 +0900

    Discord request

--- utils.js
+++ utils.js
@@ -1,6 +1,26 @@
 import 'dotenv/config';
 
 async function DiscordRequest(method, endpoint, options) {
+  const url = `https://discord.com/api/v10/${endpoint}`;
+
+  if (options.body) {
+    options.body = JSON.stringify(options.body);
+  }
+
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      'Content-Type': 'application/json; charset=utf-8',
+      'User-Agent': 'SoaringTechBot',
+    }
+    ...options
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    throw new Error(JSON.stringify(data));
+  }
+
   return res;
 }
 
