c2cc8df454ca42d63c6a92660fe73b9cb9558fdc
munhyunsu <munhyunsu@gmail.com>
2024-08-31 22:16:51 +0900

    module via type: module

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
-const express = require('express');
-
+import 'dotenv/config';
+import express from 'express';
 
 const app = express();
 const port = process.env.EXPRESS_APP_PORT || 3000;
@@ -10,7 +9,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.json({'message': 'Hello, world!'});
 });
-
 
 app.use(`${process.env.EXPRESS_APP_URL_PREFIX}`, router);
 
